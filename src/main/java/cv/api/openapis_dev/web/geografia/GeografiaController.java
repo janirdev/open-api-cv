171244725ce2393eee1e9892cfc090cb483335a2
janir <janiralves10@gmail.com>
2026-03-16 16:16:38 -0100

    fix

--- src/main/java/cv/api/openapis_dev/web/geografia/GeografiaController.java
+++ src/main/java/cv/api/openapis_dev/web/geografia/GeografiaController.java
@@ -21,19 +21,19 @@ public class GeografiaController {
     private final GeografiaService geografiaService;
 
     @GetMapping("/nacionalidade")
-    @Operation(summary = "Lista todas as nacionalidades")
+    @Operation(summary = "List all nationalities")
     public List<ComboboxDto> getNacionalidade(){
         return geografiaService.getNacionalidade();
     }
 
     @GetMapping("/localidade")
-    @Operation(summary = "Lista localidades por tipo e idPai")
+    @Operation(summary = "List of locations by type and parent ID")
     public List<ComboboxDto> getNacionalidade(@RequestParam("tipo") String tipo, @RequestParam("idPai") String idPai){
         return geografiaService.getLocalidadeByPai(tipo, idPai);
     }
 
     @GetMapping("/delalhe/{idLocalidade}")
-    @Operation(summary = "Lista localidades por tipo e idPai")
+    @Operation(summary = "Details of a location")
     public LocalidadeDto getNacionalidade(@RequestParam("idLocalidade") String idLocalidade){
         return geografiaService.getDetalheLocalidade(idLocalidade);
     }
